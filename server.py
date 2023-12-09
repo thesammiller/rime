9c15a65dd673455448e115f1ee97417e962b4ba5
Sam Miller <samuelsmiller@gmail.com>
2023-12-09 02:44:06 +0000

    Text area.

--- server.py
+++ server.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def form():
     return render_template('form.html')
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+    say_greeting=request.form['say']
+    to_name=request.form['to']
+    comments=request.form['comments']
+    return render_template('greeting.html', say=say_greeting, to=to_name, comments=comments)
 
 if __name__ == "__main__":
     app.run(app.run(debug=True, host="0.0.0.0", port=8000))
