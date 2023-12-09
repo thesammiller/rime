048b1a8b1122ca75e579ec04743a03a162d3822c
Sam Miller <samuelsmiller@gmail.com>
2023-12-09 03:13:20 +0000

    Update to communicate with OpenAI.

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request
+import os
+from openai import OpenAI
 
 app = Flask(__name__)
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+client = OpenAI(
+    api_key=OPENAI_API_KEY,
+)
+
+CHATGPT_MODEL="gpt-3.5-turbo"
+
+messages = [ {"role": "system", "content":  
+              "You are a intelligent assistant."} ] 
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -10,10 +21,19 @@ def form():
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
+    message = request.form['comments']
+    messages.append( 
+            {"role": "user", "content": message}, 
+        )
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model="gpt-3.5-turbo",
+    )
+    reply = chat_completion.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
     say_greeting=request.form['say']
     to_name=request.form['to']
-    comments=request.form['comments']
-    return render_template('greeting.html', say=say_greeting, to=to_name, comments=comments)
+    return render_template('greeting.html', comments=reply)
 
 if __name__ == "__main__":
     app.run(app.run(debug=True, host="0.0.0.0", port=8000))
